4b0714cff93f168c5dba123971367a47a215bbed
YuRae <jennyr0406@dgu.ac.kr>
2023-05-11 11:25:13 +0900

    Repair BGMManager Merge Error
    Repair BGMManager Merge Error

--- OSS_Project_SevenEleven/Assets/Scripts/BGMManager.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/BGMManager.cs
@@ -4,49 +4,29 @@ using UnityEngine;
 
 public class BGMManager : MonoBehaviour
 {
-<<<<<<< HEAD
-    static public BGMManager instance; //싱글톤
-
-    public AudioClip[] clips; // 배경음악
-
-    private AudioSource source;
-
-    private void Awake() //Start보다 먼저 실행
-    {
-        //Scene 전환시 객체 파괴 방지 코드
-=======
-
     //*사용방법*
     //collision등을 통해 각각의 함수를 사용하여 음악을 관리
 
-    static public BGMManager instance;
-
+    static public BGMManager instance; //싱글톤
+                                       
     //Variables 
-
     //Public
     public AudioClip[] clips; //배경음악들
 
     //Private
-    private AudioSource source;
-
+    private AudioSource source; //음악 재생 위함
     private WaitForSeconds waitTime = new WaitForSeconds(0.01f); //객체 한번만 생성
 
     private void Awake() //Start보다 먼저 실행
     {
         //Scene 전환시 객체 파괴 방지 코드
         //전활할 씬의 카메라 오브젝트는 삭제 할 것!
->>>>>>> 3dbd9091b9158362f6b70101383ee0e9539f4237
-
         if (instance == null)
         {
             DontDestroyOnLoad(this.gameObject);
             instance = this;
         }
-<<<<<<< HEAD
-        else
-=======
         else if (instance != null)
->>>>>>> 3dbd9091b9158362f6b70101383ee0e9539f4237
         {
             Destroy(this.gameObject);
         }
@@ -57,9 +37,6 @@ public class BGMManager : MonoBehaviour
     {
         source = GetComponent<AudioSource>();
     }
-
-<<<<<<< HEAD
-=======
 
     //Method
     public void Play(int _playMusicTrack)
@@ -118,5 +95,5 @@ public class BGMManager : MonoBehaviour
             yield return waitTime;
         }
     }
->>>>>>> 3dbd9091b9158362f6b70101383ee0e9539f4237
+
 }
