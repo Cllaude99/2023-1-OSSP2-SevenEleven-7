8b84a01114917ab0b1e32721ed284c37597b6672
TaeYoon Kim <xodbsrla1025@naver.com>
2023-05-14 00:29:00 +0900

    add Item Script
    Item Script 생성

--- OSS_Project_SevenEleven/Assets/Scripts/DatabaseManager.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/DatabaseManager.cs
@@ -25,10 +25,14 @@ public class DatabaseManager : MonoBehaviour
     public string[] switch_name;
     public bool[] switches; // 보스 정보에 대한 스위치.
 
+    public List<Item> itemList = new List<Item>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemList.Add(new Item(10001, "부적", "왼쪽 위 부적", Item.ItemType.Quest));
+        itemList.Add(new Item(10002, "일기장", "첫번째 일기장", Item.ItemType.Quest));
+        itemList.Add(new Item(10003, "열쇠", "아이스페이스 열쇠", Item.ItemType.Quest));
     }
 
 }
