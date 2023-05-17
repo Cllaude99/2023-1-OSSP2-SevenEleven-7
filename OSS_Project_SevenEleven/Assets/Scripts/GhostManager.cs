b8424fd183d2be4085ee82a40e8375012d366c3d
nyyyyyr <127469346+YuuuuuRAE@users.noreply.github.com>
2023-05-18 02:34:59 +0900

    Modify GhostManager

--- OSS_Project_SevenEleven/Assets/Scripts/GhostManager.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/GhostManager.cs
@@ -1,27 +1,20 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.UI;
 
 public class GhostManager : MovingObject
 {
     public Transform target;
 
-    NavMeshAgent agent;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        target = GameObject.Find("Player").transform;
-        agent.destination = target.transform.position;
+        StartCoroutine(GhostMove());
     }
 
-    private void Update()
+    IEnumerator GhostMove()
     {
-        
+        transform.LookAt(target);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * walkCount);
+        yield return new WaitForSeconds(1f);
     }
 }
-    //zzzzz
