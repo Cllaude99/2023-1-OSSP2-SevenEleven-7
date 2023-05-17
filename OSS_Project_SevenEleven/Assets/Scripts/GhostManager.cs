039eab02c9fa0e046e97a4d7e475941eda6eb811
YuRae <jennyr0406@dgu.ac.kr>
2023-05-17 16:29:35 +0900

    Add NPC AI
    귀신 AI 추가

---
+++ OSS_Project_SevenEleven/Assets/Scripts/GhostManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class GhostManager : MovingObject
+{
+    public Transform target;
+
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Player").transform;
+        agent.destination = target.transform.position;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
