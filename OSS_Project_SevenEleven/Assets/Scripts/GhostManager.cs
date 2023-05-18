fbf88e5892b331c50f0ee228130429bd5d5aab72
YuRae <jennyr0406@dgu.ac.kr>
2023-05-18 19:11:33 +0900

    Modify Map & GhostManager
    귀신 움직임 애니메이션 추가 일부 맵 수정

--- OSS_Project_SevenEleven/Assets/Scripts/GhostManager.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/GhostManager.cs
@@ -14,6 +14,7 @@ public class GhostManager : MovingObject
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         StartCoroutine(GhostCoroutine());
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -31,26 +32,35 @@ public class GhostManager : MovingObject
             Vector2 direction = targetPosition - rb.position;
             direction.Normalize(); // 방향 벡터 정규화
 
-            yield return null;
+            //정규환 된 벡터를 통해 vector값 결정 (1,0,0) or (0,1,0)
+            if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            {
+                vector.x = Mathf.Sign(direction.x);
+                vector.y = 0;
+            }
+            else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+            {
+                vector.x = 0;
+                vector.y = Mathf.Sign(direction.y);
+            }
 
+            //Animation
+            animator.SetFloat("DirX", vector.x); // x벡터 값을 전달해서 animation을 실행시킴
+            animator.SetFloat("DirY", vector.y); // y벡터 값을 전달해서 animation을 실행시킴
+            animator.SetBool("Walking", true);
             while (currentWalkCount < walkCount)
             {
-                animator.SetBool("Walking", true);
+
                 if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) //x와의 거리가 더 크다면
                 {
                     //rb.velocity = new Vector2(Mathf.Sign(direction.x) * chaseSpeed, 0f);
-                    transform.Translate(Mathf.Sign(direction.x) * speed, 0, 0);
-                    //Animation
-                    animator.SetFloat("DirX", Mathf.Sign(direction.x)); // x벡터 값을 전달해서 animation을 실행시킴
-                    animator.SetFloat("DirY", 0); // y벡터 값을 전달해서 animation을 실행시킴
+                    transform.Translate(vector.x * speed, 0, 0);
                 }
                 else
                 {
                     //rb.velocity = new Vector2(0f, Mathf.Sign(direction.y) * chaseSpeed);
-                    transform.Translate(0, Mathf.Sign(direction.y) * speed, 0);
-                    //Animation
-                    animator.SetFloat("DirX", 0); // x벡터 값을 전달해서 animation을 실행시킴
-                    animator.SetFloat("DirY", Mathf.Sign(direction.y)); // y벡터 값을 전달해서 animation을 실행시킴
+                    transform.Translate(0, vector.y * speed, 0);
+
                 }
                 currentWalkCount++;
                 yield return new WaitForSeconds(0.01f);
