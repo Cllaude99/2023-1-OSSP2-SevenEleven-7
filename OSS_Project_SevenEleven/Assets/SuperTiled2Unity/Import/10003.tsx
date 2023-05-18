baede221e99982520b68b0658c8d2b26b0f5d5b9
YuRae <jennyr0406@dgu.ac.kr>
2023-05-18 19:18:10 +0900

    Repair TileMap Error
    아리수 타일맵 에러 해결

---
+++ OSS_Project_SevenEleven/Assets/SuperTiled2Unity/Import/10003.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="10003" tilewidth="4" tileheight="4" tilecount="625" columns="25">
+ <image source="10003.png" width="100" height="100"/>
+</tileset>
