57addfe239214c1beb287c01e63b25788d86d6e0
YuRae <jennyr0406@dgu.ac.kr>
2023-05-18 17:28:08 +0900

    Add Ghost Move
    귀신 추적 ai 추가

--- OSS_Project_SevenEleven/Assets/Scripts/GhostManager.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/GhostManager.cs
@@ -4,17 +4,59 @@ using UnityEngine;
 
 public class GhostManager : MovingObject
 {
-    public Transform target;
+    public Transform target; // 추격 대상(플레이어)
+    public float chaseSpeed; // 추격 속도
+
+    private Rigidbody2D rb;
 
     private void Start()
     {
-        StartCoroutine(GhostMove());
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        StartCoroutine(GhostCoroutine());
     }
 
-    IEnumerator GhostMove()
+    private void Update()
     {
-        transform.LookAt(target);
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * walkCount);
-        yield return new WaitForSeconds(1f);
+    }
+
+    IEnumerator GhostCoroutine()
+    {
+        while (true)
+        {
+            // 추격 대상의 위치 가져오기
+            Vector2 targetPosition = target.position;
+
+            // 추격자와 추격 대상 사이의 방향 계산
+            Vector2 direction = targetPosition - rb.position;
+            direction.Normalize(); // 방향 벡터 정규화
+
+            yield return null;
+
+            while (currentWalkCount < walkCount)
+            {
+                animator.SetBool("Walking", true);
+                if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) //x와의 거리가 더 크다면
+                {
+                    //rb.velocity = new Vector2(Mathf.Sign(direction.x) * chaseSpeed, 0f);
+                    transform.Translate(Mathf.Sign(direction.x) * speed, 0, 0);
+                    //Animation
+                    animator.SetFloat("DirX", Mathf.Sign(direction.x)); // x벡터 값을 전달해서 animation을 실행시킴
+                    animator.SetFloat("DirY", 0); // y벡터 값을 전달해서 animation을 실행시킴
+                }
+                else
+                {
+                    //rb.velocity = new Vector2(0f, Mathf.Sign(direction.y) * chaseSpeed);
+                    transform.Translate(0, Mathf.Sign(direction.y) * speed, 0);
+                    //Animation
+                    animator.SetFloat("DirX", 0); // x벡터 값을 전달해서 animation을 실행시킴
+                    animator.SetFloat("DirY", Mathf.Sign(direction.y)); // y벡터 값을 전달해서 animation을 실행시킴
+                }
+                currentWalkCount++;
+                yield return new WaitForSeconds(0.01f);
+            }
+            currentWalkCount = 0;
+            animator.SetBool("Walking", false);
+        }
     }
 }
