e3dfb53cbb468a45a048113f6408d0cff2a233ab
TaeYoon Kim <xodbsrla1025@naver.com>
2023-05-20 13:57:07 +0900

    add SaveNLoad

--- OSS_Project_SevenEleven/Assets/Scripts/SaveNLoad.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/SaveNLoad.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.SceneManagement;
 
 public class SaveNLoad : MonoBehaviour
 {
@@ -23,8 +26,8 @@ public class SaveNLoad : MonoBehaviour
         public List<float> varNumberList;
     }
 
-    private PlayerManager thePlayer; // 플레이어의 위치값을 알기위한 변수
     private DatabaseManager theDatabase;
+    private PlayerManager thePlayer; // 플레이어의 위치값을 알기위한 변수
     private Inventory theInven;
 
     public Data data;
@@ -37,10 +40,105 @@ public class SaveNLoad : MonoBehaviour
         theDatabase = FindObjectOfType<DatabaseManager>();
         thePlayer = FindObjectOfType<PlayerManager>();
         theInven = FindObjectOfType<Inventory>();
+
+        data.playerX = thePlayer.transform.position.x;
+        data.playerY = thePlayer.transform.position.y;
+        data.playerZ = thePlayer.transform.position.z;
+
+        data.mapName = thePlayer.currentMapName;
+        data.sceneName = thePlayer.currentSceneName;
+
+        Debug.Log("기초 데이터 성공");
+
+        data.playerItemInventory.Clear();
+        data.playerItemInventoryCount.Clear();
+
+        for(int i=0; i < theDatabase.var_name.Length; i++)
+        {
+            data.varNameList.Add(theDatabase.var_name[i]);
+            data.varNumberList.Add(theDatabase.var[i]);
+        }
+        for (int i = 0; i < theDatabase.switch_name.Length; i++)
+        {
+            data.swNameList.Add(theDatabase.switch_name[i]);
+            data.swList.Add(theDatabase.switches[i]);
+        }
+
+        List<Item> itemList = theInven.SaveItem();
+
+        for(int i=0;i<itemList.Count; i++)
+        {
+            Debug.Log("인벤토리의 아이템 저장 완료 : " + itemList[i].itemID);
+            data.playerItemInventory.Add(itemList[i].itemID);
+            data.playerItemInventoryCount.Add(itemList[i].itemCount);
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.dataPath + "/SaveFile.dat");
+
+        bf.Serialize(file, data);
+        file.Close();
+
+        Debug.Log(Application.dataPath + "의 위치에 저장했습니다.");
     }
 
     public void CallLoad() // 로드가 이루어지는 역할
     {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.dataPath + "/SaveFile.dat", FileMode.Open);
 
+        if(file != null && file.Length > 0)
+        {
+            data = (Data)bf.Deserialize(file);
+
+            theDatabase = FindObjectOfType<DatabaseManager>();
+            thePlayer = FindObjectOfType<PlayerManager>();
+            theInven = FindObjectOfType<Inventory>();
+
+            thePlayer.currentMapName = data.mapName;
+            thePlayer.currentSceneName = data.sceneName;
+
+            vector.Set(data.playerX, data.playerY, data.playerZ);
+            thePlayer.transform.position = vector;
+
+            theDatabase.var = data.varNumberList.ToArray();
+            theDatabase.var_name = data.varNameList.ToArray();
+            theDatabase.switches = data.swList.ToArray();
+            theDatabase.switch_name = data.swNameList.ToArray();
+
+            List<Item> itemList = new List<Item>();
+
+            for(int i=0; i < data.playerItemInventory.Count; i++)
+            {
+                for(int x = 0; x < theDatabase.itemList.Count; x++)
+                {
+                    if(data.playerItemInventory[i] == theDatabase.itemList[x].itemID)
+                    {
+                        itemList.Add(theDatabase.itemList[x]);
+                        Debug.Log("인벤토리 아이템을 로드했습니다 : " + theDatabase.itemList[x].itemID);
+                        break;
+                    }
+                }
+            }
+
+            for(int i=0;i<data.playerItemInventoryCount.Count; i++)
+            {
+                itemList[i].itemCount = data.playerItemInventoryCount[i];
+            }
+
+            theInven.LoadItem(itemList);
+
+            // 현재 씬과 다른씬에 있는 객체들은 참조 불가능이기 때문에 씬이동이 이루어지고 난 후 그 씬에 붙어잇는 맵의 바운드를 참조함
+            GameManager theGM = FindObjectOfType<GameManager>();
+            theGM.LoadStart();
+
+            SceneManager.LoadScene(data.sceneName); 
+        }
+        else
+        {
+            Debug.Log("저장된 세이브 파일이 없습니다.");
+        }
+
+        file.Close();
     }
 }
