c371dada9bb3ae2921cfdea0d5f2bc7b2d01170c
수민 <127471190+eesoomiin@users.noreply.github.com>
2023-06-01 03:50:09 +0900

    modify startsene

--- OSS_Project_SevenEleven/Assets/Scripts/LightController.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/LightController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
+    private PlayerManager thePlayer; //플레이어가 바라보고 있는 방향
+     private Vector2 vector;
+
+    private Quaternion rotation;//회전(각도)을 담당하는 Vector4 xyzw
+
     // Start is called before the first frame update
     void Start()
     {
+        thePlayer = FindObjectOfType<PlayerManager>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        vector.Set(thePlayer.animator.GetFloat("DirX"), thePlayer.animator.GetFloat("DirY"));
     }
 }
