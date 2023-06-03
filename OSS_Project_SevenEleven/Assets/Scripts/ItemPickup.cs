ed95aa9c4c8d9cbf8f71418098baf40d4f983726
Wonjun Hong <hwjun12@naver.com>
2023-06-03 09:19:00 +0900

    show,invoke diary

--- OSS_Project_SevenEleven/Assets/Scripts/ItemPickup.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/ItemPickup.cs
@@ -7,8 +7,10 @@ public class ItemPickup : MonoBehaviour
     public int itemID; // 데이터베이스의 ItemID와 비교한다. 일치하는 경우 인벤토리에 추가함
     public int _count;
     public string pickUpSound; // 아이템 획득시 사운드
-
+    private OrderManager theOrder;
+ 
     public bool isPick = false;
+    public GameObject displayImage;
 
     /*private void OnTriggerStay2D(Collider2D collision)
     {   
@@ -21,7 +23,9 @@ public class ItemPickup : MonoBehaviour
             isPick = true;
         }
     }*/
+
     private bool isPlayerOn = false;                        // 박스 콜라이더와 플레이어가 겹쳐있는지 확인하는 변수
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))                 //플레이어가 박스 콜라이더 위에 있으면
@@ -34,9 +38,26 @@ public class ItemPickup : MonoBehaviour
             isPlayerOn = false;
     }
 
+    private void ShowItemImage()                            //아이템 상세사진 플로팅 (일기장 사진 등)
+    {
+        theOrder.NotMove();
+        displayImage.SetActive(true);
+    }
+
+    private void HideItemImage()                            //아이템 상세사진 감춤 (일기장 사진 등)
+    {
+        displayImage.SetActive(false);
+        theOrder.Move();
+    }
+
+    void Start()
+    {
+        theOrder = FindObjectOfType<OrderManager>();
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X)) 
+        if (Input.GetKey(KeyCode.X)) 
         {
             if (isPlayerOn)                                     //X키 누르고 playeron이면
             {
@@ -47,10 +68,11 @@ public class ItemPickup : MonoBehaviour
                 isPick = true;
             }
 
-
+            if (10006 <= itemID && itemID <= 10015 && displayImage != null)   //일기장 획득하면 자동으로 화면에 나오도록
+            {
+                ShowItemImage();
+                Invoke("HideItemImage", 8);
+            }
         }
-
     }
-
-
 }
