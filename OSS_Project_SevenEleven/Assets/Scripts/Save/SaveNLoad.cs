d5629da4bf2bedc9f388b89b6107177e43ac3dde
Wonjun Hong <hwjun12@naver.com>
2023-06-16 03:33:14 +0900

    update dialoguemanager

--- OSS_Project_SevenEleven/Assets/Scripts/Save/SaveNLoad.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/Save/SaveNLoad.cs
@@ -282,7 +282,7 @@ public class SaveNLoad : MonoBehaviour
                 {
                     foreach(Transform child in items.transform)
                     {
-                        if (child.name == theDatabase.itemList[x].itemID.ToString() && child.name != "10026" && child.name != "10027" && child.name != "10029")
+                        if (child.GetComponent<ItemPickup>().itemID == theDatabase.itemList[x].itemID && child.name != "10026" && child.name != "10027" && child.name != "10029")
                         {
                             child.gameObject.SetActive(true);
                         }
