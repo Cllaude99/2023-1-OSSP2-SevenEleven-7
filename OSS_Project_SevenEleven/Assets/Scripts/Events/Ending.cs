736024bbcaf4d23f0a2d7f42d110258cedf05a41
kivccc <g7donut@dgu.ac.kr>
2023-06-15 19:45:06 +0900

    Update Ending.cs

--- OSS_Project_SevenEleven/Assets/Scripts/Events/Ending.cs
+++ OSS_Project_SevenEleven/Assets/Scripts/Events/Ending.cs
@@ -6,16 +6,31 @@ public class Ending : MonoBehaviour
 {
     public GameObject go;
     private OrderManager theOrder;
+    private Camera theCamera;
+    public TitleButton thetitle;
+    private FadeManager theFade;
 
     void Start()
     {
+        theCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
         theOrder = FindObjectOfType<OrderManager>();
+        thetitle = FindObjectOfType<TitleButton>();
+        theFade = FindObjectOfType<FadeManager>();
 
     }
     public void OnTriggerStay2D(Collider2D collision)
     {
             theOrder.NotMove();
             go.SetActive(true);
- 
+            Invoke("OpenTitleAfterEnding", 20f);
+    }
+
+    public void OpenTitleAfterEnding()
+    {
+        theFade.FadeIn();
+        theCamera.GetComponent<CameraManager>().enabled = true;            //카메라 쉐이크에서꺼진거 다시  true
+        thetitle.OpenTitleUI();
+        go.SetActive(false);
+
     }
 }
